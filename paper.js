9d80023f086b9199d4ff16da25e3eb0d5da83813
DakshPatel2007 <79900403+DakshPatel2007@users.noreply.github.com>
2021-04-06 16:37:15 +1000

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,32 @@
+class Paper{
+	constructor(x,y,r) {
+		var options = {
+            isStatic:false,
+			'restituition':0.3,
+			'friction':0,
+			'density':1.2
+			
+		}
+    {
+	
+		this.x=x;
+		this.y=y;
+		this.r=r;
+		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options);
+		this.image = loadImage("paper.png");
+
+		World.add(world, this.body);
+	}
+	}
+	display(){
+			var rubberpos=this.body.position;		
+			push()
+			translate(rubberpos.x, rubberpos.y);
+			imageMode(CENTER)
+			image(this.image,0,0,this.r, this.r);
+			
+
+			pop()
+	}
+};
+
